07e63b682b4b9f015f5c731781484547b49054db
Mukhammadkarim's MacBook Pro <macbook@Muhammadkarims-MacBook-Air.local>
2023-04-23 10:53:50 +0500

    Updating

--- app.js
+++ app.js
@@ -95,7 +95,126 @@
 // const numbers = [1, 2, 3, 4, 5, 6, 7];
 // const numbers2 = numbers.slice(-4);
 
-// SPLICE 
+// SPLICE
 
 // const numbers = [1, 2, 3, 4];
 // const deleted = numbers.splice(2, 1);
+
+// SORT
+
+// const names = ['Sam', 'Jam', 'Ram', 'Dam', 'Bam'];
+// names.sort();
+// output: ['Bam', 'Dam', 'Jam', 'Ram', 'Sam']
+//---------------
+// const numbers = [76, 18, 29, 109, 4];
+// numbers.sort(comparing);
+// function comparing(a, b) {
+// 	// 1. < 0 ... a comes first.
+// 	// 2. 0 ... nothing will be changed.
+// 	// 1. > 0 ... b comes first.
+// 	return a - b;
+// }
+// out: [4, 18, 29, 76, 109]
+//-----------------------
+// const store = [
+// 	{ product: "laptop", price: 1000, },
+// 	{ product: "desktop", price: 1500, },
+// 	{ product: "mobile", price: 500, },
+// ];
+// store.sort((a, b) => a.price - b.price);
+
+// CONCAT
+
+// const a = [1, 2, 3];
+// const b = [4, 5, 6];
+// const c = [7, 8, 9];
+// const d = a.concat(b, c);
+// out: [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+// FILL
+
+// const numbers = [76, 18, 29, 109, 4, 90, 1];
+// numbers.fill(0);
+// out: [0, 0, 0, 0, 0, 0, 0];
+// numbers.fill(0, 2, 5);
+// out: [76, 18, 0, 0, 0, 90, 1];
+//--------------------
+// function fillInNumbers(n) {
+//   return Array(n)
+//     .fill(0)
+//     .map((val, inx) => inx + 1);
+// }
+// out: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// INCLUDES
+
+// const fruits = ["Apple", "Orange", "Mango"];
+// const res = fruits.includes("Orange");
+// if (res) {
+// 	console.log("True");
+// } else {
+// 	console.log("False");
+// }
+//out: True
+
+// REVERSE
+
+// const numbers = [1, 2, 3, 4, 5];
+// numbers.concat();
+// out: [5, 4, 3, 2, 1];
+//-----------------
+// const str = 'Coding is fun!';
+// const res = str.split('').reverse().join('');
+// out: '!nuf si gnidoC'
+
+// PUSH
+
+// const numbers = [1, 2, 3, 4, 5];
+// numbers.push(6)
+// out: [1, 2, 3, 4, 5, 6]
+
+// POP
+
+// const numbers = [1, 2, 3, 4, 5];
+// numbers.pop();
+// out: [1, 2, 3, 4]
+
+// UNSHIFT
+
+// const numbers = [1, 2, 3, 4];
+// numbers.unshift(2);
+// out: [2, 1, 2, 3, 4];
+
+//SHIFT
+
+// const numbers = [1, 2, 3, 4];
+// numbers.shift();
+// out: [2, 3, 4]
+
+//indexOf
+
+// const names = ['Florin', 'Ivan', 'Liam'];
+// const idx = names.indexOf('Ivan');
+// names[idx] = 'Jay'
+// out: ['Florin', 'Jay', 'Liam'];
+
+// EVERY
+
+// const numbers = [1, 2, 3, 4, 5];
+// const res = numbers.every(isPositive);
+// function isPositive(item) {
+// 	return item > 0;
+// }
+// out: true;
+//------------------------
+// const persons = [{ name: "Anwar" }, { name: "Islom" }, { name: "Sardor" }, { surname: "Jay" }];
+// const res = persons.every(person => person.name !== undefined);
+// out: false;
+//--------------------
+// const arrays = [
+// 	[1, 2, 3],
+// 	[4, 5, 6],
+// 	[7, 8, 9]
+// ];
+// const res = arrays.every(arr => Array.isArray(arr));
+// out: true;
