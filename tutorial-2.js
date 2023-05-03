0c1e76d642ae2a80c6618f466073b8c208f8bf68
Muhammadkarim-98 <105283322+Muhammadkarim-98@users.noreply.github.com>
2023-05-03 17:45:24 +0000

    updated

--- tutorial-2.js
+++ tutorial-2.js
@@ -15,3 +15,28 @@ const str = 'code explain is code !';
 // console.log(str.endsWith('!')) out: true;
 // const str2 = 'Good code is good and ';
 // console.log(str2.concat(str)) out: Good code is good and code explain is code!;
+// console.log(str.repeat(2)) out: code explain is code!code explain is code!
+// console.log(str.trim()) --> The trim() method removes whitespace from both ends of a string and returns a new string, without modifying the original string. <--
+// console.log(str.trimStart())
+// console.log(str.trimEnd())
+// console.log(str.padStart(3));
+// console.log(str.padEnd(3));
+// const a = 'réservé';
+// const b = 'RESERVE';
+// console.log(a.localeCompare(b));out: 1
+// console.log(a.localeCompare(b, 'en', { sensitivity: 'base' }));out: 0
+// console.log(str.search(/is/)) out: 13;
+// console.log(str.match('ain'));
+// out: ['ain', index: 9, input: 'code explain is code !', groups: undefined]
+// console.log(str.match(/ain/));
+// out: ['ain', index: 9, input: 'code explain is code !', groups: undefined]
+// console.log(str.match(/ain/g)); out: ['ain'];
+// console.log(str.replace('code', 'si'));
+// out: si explain is code !
+// console.log(str.replace('code', (str) => { return str.toUpperCase() })); out: CODE explain is code!
+// console.log(str.replaceAll(/code/, 'some')); out: TypeError: String.prototype.replaceAll called with a non - global RegExp argument;
+// console.log(str.charCodeAt(1)) out:111;
+// console.log(str.charCodeAt(2)) out:100;
+// console.log(str.charCodeAt(10)) out:105;
+// let t = new String('hey man');
+// console.log(t.toString()) out: hey man;
